7999c8f7437880744eb5fa8000551835148c0eb8
Asmat <asmat@mail.com>
2025-10-06 20:32:33 +0530

    Update all files

--- webhook_listener/webhook.py
+++ webhook_listener/webhook.py
@@ -3,10 +3,9 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route('/alert', methods=['POST'])
-def alert():
-    subprocess.run(['ansible-playbook', 'restart-nginx.yml'])
-    return 'NGINX restart triggered', 200
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    subprocess.run(["ansible-playbook", "ansible/restart_nginx.yml"])
+    return "Triggered", 200
 
-if __name__ == '__main__':
-    app.run(port=5001)
+app.run(port=5001)
