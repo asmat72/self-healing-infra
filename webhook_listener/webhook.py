4f58e0bbae8fe78c5251df819c6a09a5932b431d
Asmat <asmat@mail.com>
2025-10-07 21:11:40 +0530

    Update all files

--- webhook_listener/webhook.py
+++ webhook_listener/webhook.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    print("Received alert:", request.json)
-    subprocess.run(["ansible-playbook", "ansible/restart-nginx.yml"])
-    return '', 200
+        try:
+            result = subprocess.run(
+                ["ansible-playbook", "ansible/restart_nginx.yml"],
+                check=True,
+                capture_output=True,
+                text=True
+            )
+            return f"Triggered successfullyy:\n{result.stdout}", 200
+        except subprocess.CalledProcessError as e:
+            return f"Error triggering playbook:\n{e.stderr}", 500
 
-app.run(port=5000)
+if __name__ == "__main__"
+    # Use 0.0.0.0 to listen on all interfaces
+    app.run(port=5000)
